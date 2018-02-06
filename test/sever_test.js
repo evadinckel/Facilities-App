cf1ab15f300cd25b53dc477ec236e1f4085a725a
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2018-02-06 13:17:51 +0000

    Fixed tests to actual assertions

--- test/sever_test.js
+++ test/sever_test.js
@@ -11,23 +11,42 @@ describe('server access', () =>{
 
 
   describe('GET /vote_hot route', ()=> {
+    it('works', function(done){
     chai.request(server)
       .get('/vote_hot')
       .end((err,res)=> {
         res.should.have.status(200);
+                console.log(res.text)
         res.text.should.be.eql('1');
-      // done();
+        done();
       });
+    });
   });
 
   describe('GET /vote_cold route', ()=> {
+    it('works', function(done){
     chai.request(server)
       .get('/vote_cold')
       .end((err,res)=> {
         res.should.have.status(200);
+                console.log(res.text)
         res.text.should.be.eql('0');
-      // done();
+      done();
       });
+    });
+  });
+
+  describe('GET /vote_neutral route', ()=> {
+    it('works', function(done){
+    chai.request(server)
+      .get('/vote_neutral')
+      .end((err,res)=> {
+        res.should.have.status(200);
+        console.log(res.text)
+        res.text.should.eql('0')
+      done();
+      });
+    });
   });
 
 });
