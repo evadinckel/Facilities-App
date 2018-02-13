20130086ab533ed9ac02abcee4969b619543a593
Nero <neroshan12@gmail.com>
2018-02-13 15:34:06 +0000

    Add cold and neutral routes

--- src/server.js
+++ src/server.js
@@ -1,40 +1,52 @@
 import express from 'express';
-import Counter from "../src/counterModel.js";
-const app = express()
-const counter = new Counter()
+import Counter from '../src/counterModel.js';
+const app = express();
+const counter = new Counter();
 
 // app.use(function (req, res, next) {
 //     // Website you wish to allow to connect
 //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 // })
 
-app.get('/vote_hot', function (req, res, next) {
- counter.voteHot()
- console.log(counter.hotCounter)
-  next()
-}, function (req, res) {
-  // res.send(`${voteCounter.currentVote}`)
- res.send({ votes: counter.hotCounter });
-})
+app.get(
+  '/vote_hot',
+  function(req, res, next) {
+    counter.voteHot();
+    console.log(counter.hotCounter);
+    next();
+  },
+  function(req, res) {
+    res.send({ votes: counter.hotCounter });
+  }
+);
 
-// app.get('/vote_cold', function (req, res, next) {
-//  counter.voteCold()
-//   next()
-// }, function (req, res) {
-//   res.send(`${voteCounter.currentVote}`)
-// })
+app.get(
+  '/vote_cold',
+  function(req, res, next) {
+    counter.voteCold();
+    next();
+  },
+  function(req, res) {
+    res.send({ votes: counter.coldCounter });
+  }
+);
 
-// app.get('/vote_neutral', function (req, res, next) {
-//   counter.voteNeutral()
-//   next()
-// }, function (req, res) {
-//   res.send(`${voteCounter.currentVote}`)
-// })
+app.get(
+  '/vote_neutral',
+  function(req, res, next) {
+    counter.voteNeutral();
+    next();
+  },
+  function(req, res) {
+    res.send({ votes: counter.neutralCounter });
+  }
+);
 
-var server = app.listen(4000, ()=> console.log('Example app listening on port 4000!'))
+var server = app.listen(4000, () =>
+  console.log('Example app listening on port 4000!')
+);
 
 module.exports = {
   server: server,
   app: app
-
-}
+};
