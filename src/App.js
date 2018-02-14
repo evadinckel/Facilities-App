dcf4a1f089966718c4335b2579063dee587bef87
Eva.Dinckel <evadinckel@gmail.com>
2018-02-14 13:01:27 +0000

    add a state to App(username for cookie)

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import Wrapper from './Wrapper.js';
 
 
 class App extends Component {
-
-  componentDidMount() {
-    // let time = new Date()
-    // let secs = time.getTime()
-    // let cookie = document.cookie = `username=${secs}`
-    console.log(document.cookie)
-  // console.log(cookie)
+  constructor(props) {
+    super(props);
+  this.state = {
+    username: this.componentDidMount()
+  };
+  console.log(this.state.username)
 }
 
+
+  componentDidMount() {
+    if (document.cookie.includes('username') === false) {
+    const time = new Date()
+    const secs = time.getTime()
+    document.cookie = `username=${secs}`
+  } else {
+    return document.cookie
+  }
+
+}
   render() {
     return (
       <div>
