313bfddcd9c7e33b2d65fb270bf36933b9f40d85
Eva.Dinckel <evadinckel@gmail.com>
2018-02-15 15:24:56 +0000

    tests for counterModel in progress

---
+++ test/__tests__/counterModel_test.js
@@ -0,0 +1,26 @@
+import Counter from '../../src/counterModel'
+
+const counter = new Counter()
+
+
+describe('Counter', () => {
+  describe('#constructor', () =>{
+    it('should start all counters at 0', () => {
+      expect(counter.hotCounter).toEqual(0)
+      expect(counter.coldCounter).toEqual(0)
+      expect(counter.neutralCounter).toEqual(0)
+    })
+    it('should start with an empty array', () => {
+      expect(counter.cookieIDLog.length).toEqual(0)
+    })
+  })
+
+describe('#vote hot', () =>{
+  it('should increase the hotCounter by 1', ()=> {
+    counter.voteHot()
+    expect(counter.hotCounter).toEqual(1)
+  })
+})
+
+
+})
